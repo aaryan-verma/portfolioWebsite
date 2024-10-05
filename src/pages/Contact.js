ed89356cf1cdca76a07fd84bb7820a8a7ddcabe4
aaryan-verma <aryanverma.av2000@gmail.com>
2024-10-05 08:54:41 +0530

    style: implemented loader using react-spinnes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import '../styles/Contact.css'
-import {MdOutlineEmail} from 'react-icons/md';
-import {FiLinkedin} from 'react-icons/fi';
-import {GrInstagram} from 'react-icons/gr';
-import { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import '../styles/Contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { FiLinkedin } from 'react-icons/fi';
 import emailjs from 'emailjs-com';
 import Button from '@material-ui/core/Button';
+import PropagateLoader from 'react-spinners/PropagateLoader'; // Import PropagateLoader
+
 const Contact = () => {
-  const form=useRef();
+  const form = useRef();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -17,36 +27,47 @@ const Contact = () => {
       }, (error) => {
           console.log(error.text);
       });
-      e.target.reset()
+    e.target.reset();
   };
+
   return (
     <section id='contact'>
-      <h2 id = 'connect-h2'>Let's Connect</h2>
-      <div className='container contact__container'>
-        <div className='contact__options'>
-          <article className='contact__option'>
-            <MdOutlineEmail className="contact__option-icon"/>
-            <h4>Email</h4>
-            <h5>aryanverma.av2000@gmail.com</h5>
-            <a href='mailto:aryanverma.av2000@gmail.com' target="_blank">Send a Message</a>
-          </article>
-          <article className='contact__option'>
-            <FiLinkedin className='contact__option-icon'/>
-            <h4>LinkedIn</h4>
-            <h5>linkedin.com/in/aaryan-verma</h5>
-            <a href='https://www.linkedin.com/in/aaryan-verma/' target="_blank">Send a Message</a>
-          </article>
-         
+      {loading ? (
+        <div className="custom-loader">
+          <h2>Let's Connect</h2>
+          <div className="loader-container">
+            <PropagateLoader size={15} color={"#3e497a"} loading={loading} style={{opacity: 0.5}} />
+          </div>
         </div>
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder="Your Full Name" required />
-          <input type="email" name="email" placeholder="Your Email" required />
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <Button variant="contained" color = "primary" type="submit" className='contact-btn'>Send Message</Button>
-        </form>
-      </div>
+      ) : (
+        <>
+          <h2 id='connect-h2'></h2>
+          <div className='container contact__container'>
+            <div className='contact__options'>
+              <article className='contact__option'>
+                <MdOutlineEmail className="contact__option-icon"/>
+                <h4>Email</h4>
+                <h5>aryanverma.av2000@gmail.com</h5>
+                <a href='mailto:aryanverma.av2000@gmail.com' target="_blank" rel="noopener noreferrer">Send a Message</a>
+              </article>
+              <article className='contact__option'>
+                <FiLinkedin className='contact__option-icon'/>
+                <h4>LinkedIn</h4>
+                <h5>linkedin.com/in/aaryan-verma</h5>
+                <a href='https://www.linkedin.com/in/aaryan-verma/' target="_blank" rel="noopener noreferrer">Send a Message</a>
+              </article>
+            </div>
+            <form ref={form} onSubmit={sendEmail}>
+              <input type="text" name="name" placeholder="Your Full Name" required />
+              <input type="email" name="email" placeholder="Your Email" required />
+              <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+              <Button variant="contained" color="primary" type="submit" className='contact-btn'>Send Message</Button>
+            </form>
+          </div>
+        </>
+      )}
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PulseLoader from 'react-spinners/PulseLoader';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 import '../styles/Home.css';
-import { FiDownload } from 'react-icons/fi';
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { MdArrowForward } from 'react-icons/md';
@@ -21,67 +21,96 @@ const skillsData = [
 ];
 
 function Home() {
+  const [loadingStates, setLoadingStates] = useState(Array(skillsData.length).fill(true));
+
+  useEffect(() => {
+    const timers = skillsData.map((_, index) =>
+      setTimeout(() => {
+        setLoadingStates(prevStates => {
+          const newStates = [...prevStates];
+          newStates[index] = false;
+          return newStates;
+        });
+      }, 500 + index * 100)
+    );
+
+    return () => timers.forEach(timer => clearTimeout(timer));
+  }, []);
+
   const renderSkills = (skills) => {
     return skills.map((skill, index) => (
       <div key={index} className='item'>
-        <h1>{skill.title}</h1>
-        <span>{skill.content}</span>
+        {loadingStates[index] ? (
+          <div className="loader" style={{ opacity: 0.5 }}>
+            <PulseLoader 
+              size={10}
+              color={"#123abc"}
+              loading={loadingStates[index]}/>
+          </div>
+        ) : (
+          <>
+            <h1>{skill.title}</h1>
+            <span>{skill.content}</span>
+          </>
+        )}
       </div>
     ));
   };
 
   return (
     <div className='home'>
-      <div className='about'>
-        <div className='me'>
-          <img src={require('../assests/26904.png')} alt='Profile' />
-        </div>
-
-        <h2>Hi, I'm Aaryan</h2>
-
-        <div className='prompt'>
-          <p>A Software Developer with a passion for learning and creating.</p>
-          <a
-            href='https://drive.google.com/file/d/1XR5JKcRHmtVcR_w4PzVqbg1-NUjvFZch/view?usp=drive_link'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='resume-button'
-          >
-            <FaCloudDownloadAlt className='resume-icon' />
-            <span className='resume-text'>Download Resume</span>
-          </a>
-          <a href='https://www.linkedin.com/in/aaryan-verma/'>
-            <LinkedInIcon />
-          </a>
-          <a href='mailto:aryanverma.av2000@gmail.com'>
-            <EmailIcon />
-          </a>
-          <a href='https://github.com/aaryan-verma/'>
-            <GitHubIcon />
-          </a>
-        </div>
-      </div>
-
-      <div className='skills'>
-        <h1>Skills</h1>
-        <div className='list'>
-          <div className='row'>
-            {renderSkills(skillsData.slice(0, 3))}
+      <div className='content'>
+        <div className='about'>
+          <div className='me'>
+            <img src={require('../assests/26904.png')} alt='Profile' />
           </div>
-          <div className='row'>
-            {renderSkills(skillsData.slice(3, 6))}
-          </div>
-          <div className='row'>
-            {renderSkills(skillsData.slice(6, 9))}
+
+          <h2>Hi, I'm Aaryan</h2>
+
+          <div className='prompt'>
+            <p>A Software Developer with a passion for learning and creating.</p>
+            <a
+              href='https://drive.google.com/file/d/1XR5JKcRHmtVcR_w4PzVqbg1-NUjvFZch/view?usp=drive_link'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='resume-button'
+            >
+              <FaCloudDownloadAlt className='resume-icon' />
+              <span className='resume-text'>Download Resume</span>
+            </a>
+            <a href='https://www.linkedin.com/in/aaryan-verma/'>
+              <LinkedInIcon />
+            </a>
+            <a href='mailto:aryanverma.av2000@gmail.com'>
+              <EmailIcon />
+            </a>
+            <a href='https://github.com/aaryan-verma/'>
+              <GitHubIcon />
+            </a>
           </div>
         </div>
-      </div>
-      <div className='view-projects'>
-        <h1>View Projects</h1>
-        <p>Explore my portfolio of projects</p>
-        <Link to='/projects' className='view-projects-button'>
-          View Projects <MdArrowForward className='arrow-icon' />
-        </Link>
+
+        <div className='skills'>
+          <h1>Skills</h1>
+          <div className='list'>
+            <div className='row'>
+              {renderSkills(skillsData.slice(0, 3))}
+            </div>
+            <div className='row'>
+              {renderSkills(skillsData.slice(3, 6))}
+            </div>
+            <div className='row'>
+              {renderSkills(skillsData.slice(6, 9))}
+            </div>
+          </div>
+        </div>
+        <div className='view-projects'>
+          <h1>View Projects</h1>
+          <p>Explore my portfolio of projects</p>
+          <Link to='/projects' className='view-projects-button'>
+            View Projects <MdArrowForward className='arrow-icon' />
+          </Link>
+        </div>
       </div>
     </div>
   );
